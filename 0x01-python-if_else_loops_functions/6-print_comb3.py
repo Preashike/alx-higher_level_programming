3691714262835be75408a16eb1a2310433702edf
preashike <ikechukwuogorchukwu8@gmail.com>
2023-10-04 16:20:42 -0700

    0x01. Python

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
-# 6-print_comb3.py
 
-"""Print all possible different combinations of two digits in ascending order.
 
-    The two digits must be different - 01 and 10 are considered identical.
+def print_uniq_combos():
+    for i in range(10):
+        for j in range(i + 1, 10):
+            if i == 8 and j == 9:
+                print('{:d}{:d}'.format(i, j))
+            else:
+                print('{:d}{:d}'.format(i, j), end=', ')
+
+
+if __name__ == '__main__':
+    print_uniq_combos()
