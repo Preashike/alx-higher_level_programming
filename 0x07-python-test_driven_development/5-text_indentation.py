2a61749116c8fa54cd5cf793fb75e7d0f857ca0d
preashike <ikechukwuogorchukwu8@gmail.com>
2023-11-02 16:05:10 -0700

    python-test_driven_development

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+
+def text_indentation(text):
+    """
+    prints a string of text with 2 new lines after '.', '?', and ':'
+    unit tests located in tests/5-text_indentation.txt
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+
+      c = o
+      while c < len(text) and text[c] == ' ':
+          c += 1   
+      while c < len(text):
+           print(text(c), end='')
+           if text[c] == "\n" or text[c] in ".?:"
+            if text[c] in ".?:":
+                print("\n")
+        c +=1
+        while c < len9text andtext[c] ==' ';
+        C += 1
+        continue
+    c+ = 1
